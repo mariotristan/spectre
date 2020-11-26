7beacbe008ad63a33915d28339bf7cf077e9933f
Mario Tristan <mariotristan@gmail.com>
2020-11-25 22:19:48 -0600

    Move code to main branch

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Spectre.Console;
+
+namespace spectre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    AnsiConsole.Markup("[underline red]Hello[/] World!");
+        }
+    }
+}
